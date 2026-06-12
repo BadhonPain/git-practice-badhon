9e945fdbd967d171a1168679fe97c83bcdfd4575
Test_Case <badhonpain48@gmail.com>
2026-06-12 09:23:26 +0600

    Zero Division Error handling

--- utils.py
+++ utils.py
@@ -8,8 +8,15 @@ def multiply(a, b):
     return a*b
 
 def divide(a, b):
+    if b==0:
+       print("Can't divide by zero !!") 
+       return 
     return a/b
+
+    
 
 print(add(5,7))
 print(subtract(5,7))
 print(multiply(5, 7))
+print(divide(5, 7))
+print(divide(2, 0))
