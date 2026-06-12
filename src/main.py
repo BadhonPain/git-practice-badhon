0e59c18b8fb387c32361cc6458e25b7dddef182f
Test_Case <badhonpain48@gmail.com>
2026-06-12 10:36:39 +0600

    Including interactivity

--- src/main.py
+++ src/main.py
@@ -8,23 +8,42 @@ print(f"Name: {name}")
 now = datetime.now()
 print("Now:", now.strftime("%Y-%m-%d %H:%M:%S"))
 
-print('''
+def printCommands():
+    print('''
         1. addition
         2. subtraction
         3. multiplication
         4. division
-      ''')
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
-operation = input("Enter operation name: ")
-if operation=="addition" :
-    print(add(a, b))
-elif operation== "subtraction":
-    print(subtract(a, b))
-elif operation== "multiplication":
-    print(multiply(a, b))
-elif operation== "division":
-    print(divide(a, b))
-else:
-    print("Invalid command! Try again")
+        5. exit
+    ''')
+
+def takeInput():
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd number: "))
+    return a, b
+
+printCommands()
+while True:
+    operation = input("Enter operation name: ")
+
+    if operation == "exit":
+        break
+    elif operation == "help":
+        printCommands()
+    elif operation == "addition":
+        a, b = takeInput()
+        print(add(a, b))
+    elif operation == "subtraction":
+        a, b = takeInput()
+        print(subtract(a, b))
+    elif operation == "multiplication":
+        a, b = takeInput()
+        print(multiply(a, b))
+    elif operation == "division":
+        a, b = takeInput()
+        print(divide(a, b))
+    else :
+        print("Invalid command !! Type 'help' to see available all commands.")
+
+
 
